2ebe4ecde2ec1db1b9c2e8e1019a500ebc4e0607
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-31 13:28:30 +0530

    Update exp11_on_pc_led_blink.c

--- exp11_on_pc_led_blink.c
+++ exp11_on_pc_led_blink.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <windows.h>  // For Sleep() function on Windows
+#include <windows.h>  
 
 int main() {
     printf("Simulating LED Blink...\n\n");
 
     for (int i = 1; i <= 3; i++) {
         printf("LED ON\n");
-        Sleep(2000);  // Wait 2 seconds (2000 milliseconds)
-
+        Sleep(2000);  
         printf("LED OFF\n");
-        Sleep(2000);  // Wait 2 seconds
+        Sleep(2000);  
     }
 
     printf("\nSimulation Complete.\n");
